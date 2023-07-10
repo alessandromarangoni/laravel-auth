6cf301b0167f50451d3498bc59d9e126132c70b0
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-07-10 20:17:32 +0200

    placeholder cards with waker content

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,14 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
+<div class="container-fluid mt-4">
     <div class="row justify-content-center">
-        <div class="col">
+        <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -18,6 +15,10 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <div class="pt-4">
+                        <a href="{{ url('admin/portfolio-show-all') }}" class="btn btn-primary">vai al portfolio</a>
+                    </div>
                 </div>
             </div>
         </div>
